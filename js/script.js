80159b21e117fd04caf689d0df5e30802eeac469
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2021-11-02 14:12:32 -0300

    Botão de Iniciar Jogo adicionado

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ function centralizarCartas(cartas){
     container.style.setProperty('--num_cartas', cartas);
     container.style.setProperty('--divisor', divisor);
 }
+function iniciarJogo(){
+    let jogo = document.querySelector(".container");
+    let menu = document.querySelector(".menu_inicial");
+
+    let num_cartas = parseInt(prompt("Insira o número de cartas desejado:"));
+    if (num_cartas > 14 || num_cartas <  4 || num_cartas % 2 !== 0){
+        num_cartas = parseInt(prompt("Por favor, número par entre 4 e 14:"))
+    }
+    menu.classList.add("escondido");
+    jogo.classList.add("jogando");
+    criarJogo(num_cartas);
+}
